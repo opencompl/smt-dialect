cafabff7d631a03a6764e695c8fb99bd357a98aa
anurudhp <anurudhp97@gmail.com>
2021-09-14 17:10:44 +0530

    add interface impl.

--- lib/SMT/SMTOps.cpp
+++ lib/SMT/SMTOps.cpp
@@ -7,3 +7,20 @@
 
 #define GET_OP_CLASSES
 #include "SMT/SMTOps.cpp.inc"
+
+using namespace mlir;
+using namespace smt;
+
+LogicalResult AssertOp::translateToSMT(raw_ostream &output) {
+  return failure();
+}
+
+LogicalResult CheckSatOp::translateToSMT(raw_ostream &output) {
+  output << "(check-sat)\n";
+  return success();
+}
+
+LogicalResult GetModelOp::translateToSMT(raw_ostream &output) {
+  output << "(get-model)\n";
+  return success();
+}
