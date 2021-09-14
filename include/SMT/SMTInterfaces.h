020d98e4abe68b6d3cb0077094988e946cae9bf9
anurudhp <anurudhp97@gmail.com>
2021-09-14 12:24:54 +0530

    Add an SMT OpInterface to support translation to SMT code.

---
+++ include/SMT/SMTInterfaces.h
@@ -0,0 +1,11 @@
+#ifndef MLIR_SMT_SMTINTERFACES_H_
+#define MLIR_SMT_SMTINTERFACES_H_
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpDefinition.h"
+
+namespace mlir {
+#include "SMT/SMTInterfaces.h.inc"
+} // namespace mlir
+
+#endif // MLIR_SMT_SMTINTERFACES_H_
---
+++ lib/SMT/SMTInterfaces.cpp
@@ -0,0 +1,6 @@
+#include "SMT/SMTInterfaces.h"
+
+using namespace mlir;
+
+// TableGen implementation file
+#include "SMT/SMTInterfaces.cpp.inc"
